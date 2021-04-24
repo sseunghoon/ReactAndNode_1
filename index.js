1a1bf050b6c3599b71bb14ed18b9bb803663c96c
sseunghoon <sseunghun99@naver.com>
2021-04-24 10:13:41 +0000

    auth

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 3000
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const { User } = require("./models/User")
+const { auth } = require("./middleware/auth")
+
 
 const config = require('./config/key')
 
@@ -23,7 +25,7 @@ mongoose.connect(config.mongoURI, {
 app.get('/',(req,res)=>res.send('Hello World!123수정내용')) // router 생성
 
 // endPoint가 /register, callBack funtion이 request, response
-app.post('/register', (req, res) => { 
+app.post('/api/users/register', (req, res) => { 
 	
 	//회원 가입 할때 필요한 정보들을 client에서 가져오면
 	//그것들을 데이터 베이스에 넣어준다.
@@ -43,7 +45,7 @@ app.post('/register', (req, res) => {
 	})	
 }) 
 
-app.post('/login',(req,res)=>{
+app.post('/api/users/login',(req,res)=>{
 	//mongoDB에서 제공하는 method
 	User.findOne({ email:req.body.email},(err,user)=>{
 		if(!user){
@@ -73,10 +75,34 @@ app.post('/login',(req,res)=>{
 		})
 		
 	})
-	
-	
-	
-	
+})
+
+//나중에 Router라는 것을 쓸때를 생각해서 /api/xxxx/를 붙여주면 좋음
+//중간에 있는 auth는 middleWare, 왼쪽(auth)이 endPoint, endPoint에서 request를 받고
+//callBack function을 실행하기전에 실행하는 것
+
+app.get('/api/users/auth', auth ,(req,res)=>{
+	//여기에 도착했다는 것은 middleWare를 통과했다는 것이고 Authentication이 True라는 뜻
+	res.status(200).json({
+		_id:req.user_id,
+		isAdmin:req.user.role === 0 ? false: true,
+		isAuth:true,
+		email:req.user.email,
+		name:req.user.name,
+		lastname:req.user.lastname,
+		role:req.user.role,
+		image:req.user.image
+	})
+})
+
+app.get('/api/users/logout', auth ,(res,req)=>{
+	User.findOneAndUpdate({_id:req.user._id},{token:""},(err,user) =>{
+		if(err) return res.json({success:false,err})
+		
+		return res.status(200).send({
+			success:true
+		})
+	}) 
 })
 
 
