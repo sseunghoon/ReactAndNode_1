cda7e35392fd827fbdbbdab5a2b13fa0d0577c3a
sseunghoon <sseunghun99@naver.com>
2021-04-24 10:18:48 +0000

    error modify

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.get('/api/users/auth', auth ,(req,res)=>{
 	})
 })
 
-app.get('/api/users/logout', auth ,(res,req)=>{
+app.get('/api/users/logout', auth ,(req,res)=>{
 	User.findOneAndUpdate({_id:req.user._id},{token:""},(err,user) =>{
 		if(err) return res.json({success:false,err})
 		
